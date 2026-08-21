69ed384f3a9980e1cbefb67c59dbbf313d54c57e
t0mnt <199419433+t0mnt@users.noreply.github.com>
2026-08-20 22:53:02 -0400

    same thing
    as previous commit in fcgp but here

--- models/modules/gp.py
+++ models/modules/gp.py
@@ -28,6 +28,8 @@ class SteerableGeometricProductLayer(nn.Module):
             self.linear_left = MVLinear(algebra, features, features, bias=True)
 
         self.product_paths = algebra.geometric_product_paths
+        self.register_buffer("_path_idx", self.product_paths.nonzero().T.contiguous(),
+                             persistent=False)
         self.weight = nn.Parameter(torch.empty(features, self.product_paths.sum()))
 
         self.reset_parameters()
@@ -42,12 +44,10 @@ class SteerableGeometricProductLayer(nn.Module):
             dtype=self.weight.dtype,
             device=self.weight.device,
         )
-        weight[:, self.product_paths] = self.weight
-        subspaces = self.algebra.subspaces
+        weight[:, :, self._path_idx[0], self._path_idx[1], self._path_idx[2]] = self.weight
+        bsi = self.algebra.blade_subspace_idx
         weight_repeated = (
-            weight.repeat_interleave(subspaces, dim=-3)
-            .repeat_interleave(subspaces, dim=-2)
-            .repeat_interleave(subspaces, dim=-1)
+            weight.index_select(-3, bsi).index_select(-2, bsi).index_select(-1, bsi)
         )
         return self.algebra.cayley * weight_repeated
 
